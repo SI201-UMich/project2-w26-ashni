5a862947a5b9238a90fd38f3413d3294970d0ce1
Ashni Pothineni <apothine@umich.edu>
2026-03-23 11:38:02 -0400

    Add header details

--- project2_starter.py
+++ project2_starter.py
@@ -1,7 +1,7 @@
 # SI 201 HW4 (Library Checkout System)
-# Your name:
-# Your student id:
-# Your email:
+# Your name: Ashni Pothineni
+# Your student id: 4559 8385
+# Your email: apothine@umich.edu
 # Who or what you worked with on this homework (including generative AI like ChatGPT):
 # If you worked with generative AI also add a statement for how you used it.
 # e.g.:
