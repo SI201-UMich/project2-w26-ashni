94350499d24e12a3253a3983793bb3509cceb46b
Ashni Pothineni <apothine@umich.edu>
2026-03-26 02:14:48 -0400

    Implement output_csv function and tests, update test cases

--- project2_starter.py
+++ project2_starter.py
@@ -224,11 +224,21 @@ def output_csv(data, filename) -> None:
     Returns:
         None
     """
-    # TODO: Implement checkout logic following the instructions
+    # Implement checkout logic following the instructions
     # ==============================
     # YOUR CODE STARTS HERE
     # ==============================
-    pass
+    header = ['Listing Title', 'Listing ID', 'Policy Number', 'Host Type', 'Host Name', 'Room Type', 'Location Rating']
+
+    # Sort data in reverse order by location rating
+    data = sorted(data, key=lambda x: x[6], reverse=True)
+
+    # Write to csv
+    with open(filename, 'w', newline='') as file:
+        csv_writer = csv.writer(file, delimiter=',')
+        csv_writer.writerow(header)     # Write header row
+        for row in data:
+            csv_writer.writerow(row)
     # ==============================
     # YOUR CODE ENDS HERE
     # ==============================
@@ -333,19 +343,25 @@ class TestCases(unittest.TestCase):
     def test_create_listing_database(self):
         # Check that each tuple in detailed_data has exactly 7 elements:
         # (listing_title, listing_id, policy_number, host_type, host_name, room_type, location_rating)
-        listing_db = create_listing_database(self.search_results_path)
-        for listing in listing_db:
+        for listing in self.detailed_data:
             self.assertEqual(len(listing), 7)
         # Spot-check the LAST tuple is ("Guest suite in Mission District", "467507", "STR-0005349", "Superhost", "Jennifer", "Entire Room", 4.8).
-        last_tuple = listing_db[-1]
+        last_tuple = self.detailed_data[-1]
         self.assertEqual(last_tuple, ("Guest suite in Mission District", "467507", "STR-0005349", "Superhost", "Jennifer", "Entire Room", 4.8))
 
     def test_output_csv(self):
         out_path = os.path.join(self.base_dir, "test.csv")
-
-        # TODO: Call output_csv() to write the detailed_data to a CSV file.
-        # TODO: Read the CSV back in and store rows in a list.
-        # TODO: Check that the first data row matches ["Guesthouse in San Francisco", "49591060", "STR-0000253", "Superhost", "Ingrid", "Entire Room", "5.0"].
+        
+        # Call output_csv() to write the detailed_data to a CSV file.
+        output_csv(self.detailed_data, out_path)
+        # Read the CSV back in and store rows in a list.
+        with open('airbnb_dataset.csv') as data:
+            reader = csv.reader(data)
+            rows = []
+            for row in reader:
+                rows.append(row)
+        # Check that the first data row matches ["Guesthouse in San Francisco", "49591060", "STR-0000253", "Superhost", "Ingrid", "Entire Room", "5.0"].
+        self.assertEqual(rows[1], ["Guesthouse in San Francisco", "49591060", "STR-0000253", "Superhost", "Ingrid", "Entire Room", "5.0"])
 
         os.remove(out_path)
 
